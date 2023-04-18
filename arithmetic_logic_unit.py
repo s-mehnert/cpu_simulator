91278f8c685c5252f86a7de0c532e7d8cf331207
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2023-04-18 13:14:37 +0200

    Initialize CPU and ALU class with basic method structure

---
+++ arithmetic_logic_unit.py
@@ -0,0 +1,11 @@
+# The code in this file will simulate the arithmetic and logic operations an arithmetic and logic unit (ALU) performs
+
+class ALU:
+    def __init__(self):
+        pass
+
+    def logic_operation(self):
+        pass
+
+    def arithmetic_operation(self):
+        pass
