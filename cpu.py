89a449224f56ad324344f8292467922818d161d8
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2023-04-20 18:06:52 +0200

    decode first part of instruction

--- cpu.py
+++ cpu.py
@@ -1,24 +1,43 @@
 # The code in this file will simulate the FETCH -> DECODE -> EXECUTE -> STORE instruction cycle a Central Processing Unit (CPU) performs
 
+from collections import deque
+
 
 class CPU:
     def __init__(self):
-        self.registers = [] * 8
         self.program_counter = None
         self.instruction_register = None
-    
+        self.registers = {"R1" : None, "R2" : None, "R3" : None, "R4" : None, "R5" : None, "R6" : None}
+            
     def fetch_instruction(self, instruction): # instructions will come in the form of a string
-        instruction.split(",")
+        print("Fetching instruction:", instruction, )
+        instruction = instruction.strip().split(",")
         if self.instruction_register == None:
             self.instruction_register = instruction
-            print("Fetching instruction.", instruction)
-            print("Loading instruction into IR", self.instruction_register)
+            print("Loading instruction into IR:", self.instruction_register)
             print("Ready for decoding.")
         else:
             print("Cannot fetch instruction as previous instruction is still waiting in line.")
 
-    def decode_instruction(self):
-        pass
+    def decode_instruction(self, is_architecture):
+        print("Decoding instruction:", self.instruction_register)
+        instruction = deque()
+        for item in self.instruction_register:
+            instruction.append(item)
+        
+        command = instruction.popleft()
+        decoded_instruction = is_architecture[command]
+        if not instruction:
+            print(decoded_instruction)
+            return decoded_instruction
+        print("So far decoded:", decoded_instruction)
+        print("Now decoding rest of instruction:", instruction)
+        destination_register = ""
+        source_register_1 = ""
+        source_register_2 = ""
+        constant = 0
+        
+        print("Ready to pass instruction to ALU", self.instruction_register)
 
     def pass_instruction_to_ALU(self):
         print("Passing instruction to ALU:", self.instruction_register)
--- simulation.py
+++ simulation.py
@@ -8,10 +8,19 @@ with open("instruction_input.txt") as instructions:
     for line in instructions.readlines():
         load_instructions.append(line)
 
+instruction_set_architecture = {
+    "CACHE" : "cache command",
+    "ADDI" : "Addition",
+    "ADD" : "Addition",
+    "J" : "jump command",
+    "HALT" : "terminate execution"
+}
+
 processor = CPU()
 
 while load_instructions:
     processor.fetch_instruction(load_instructions.popleft())
+    processor.decode_instruction(instruction_set_architecture)
     processor.pass_instruction_to_ALU()
 
 print("All instructions processed.")
